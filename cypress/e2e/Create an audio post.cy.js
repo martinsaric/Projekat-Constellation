b32e291872773cc9973e52e5a4896b01051bf6ac
martinsaric <saricmartinposao@gmail.com>
2025-05-26 21:14:16 +0200

    test: adding new test create an audio post, adjusting the Cypress commands

--- cypress/e2e/Create an audio post.cy.js
+++ cypress/e2e/Create an audio post.cy.js
@@ -8,24 +8,14 @@ describe('Create an audio post', () => {
         cy.visit('/login')
         cy.login('martinsaric94@gmail.com', 'constel123')
 
-        
-        cy.get('div.card-body').should('be.visible');
-        cy.get('input[placeholder="What\'s happening"]').type(audioPostTitle);
-        cy.get('[id="startRecordingButton"]').click()
-        cy.get('[class="vizualizerSection "]')
-        .should('be.visible')
-        cy.get('[aria-label="Stop voice recording button"]')
-        .should('be.visible')
-        .and('have.css', 'background-color', 'rgb(220, 53, 69)')
-        cy.wait(2000)
-        cy.get('[aria-label="Stop voice recording button"]').click()
-        cy.get('[aria-label="Pause voice recording"]')
-        .should('have.css', 'background-color', 'rgb(21, 126, 255)')
-        cy.contains('button[type="button"]', 'New post').click();
+        cy.createAudioPost(audioPostTitle).then(() => {
 
-        cy.get('[class="home__main__feed__post card"]')
-        .should('be.visible')
-        .and('contain', audioPostTitle)
+            cy.intercept('GET', 'https://api.hr.constel.co/api/v1/posts').as('getAudioPost')
+            cy.visit('/home')
+            cy.wait('@getAudioPost')
+            cy.get('[class="home__main__feed__post card"]')
+            .should('be.visible')
+            .and('contain', audioPostTitle)
+            })
     })
-    
-});
+})
