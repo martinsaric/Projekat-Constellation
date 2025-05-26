c84af7c6a7f9410372cad80b92f2cde75016ad48
martinsaric <saricmartinposao@gmail.com>
2025-05-26 19:22:31 +0200

    test: added test create a new comment, added new Cypress command for create a new comment

---
+++ cypress/e2e/Create a comment.cy.js
@@ -0,0 +1,23 @@
+describe('Create a comment', () => {
+
+    it('Verify that the user can create a comment', () => {
+
+        //Step 1: Cypress command to login with the user credentials
+        cy.login('martinsaric94@gmail.com', 'constel123')
+
+        //Step 2: Cypress command to create a new post
+        cy.createNewPost().then(postTitle => {
+            
+        //Step 3: Cypress command to create a new comment
+        cy.createNewComment(postTitle)
+
+        //Step 4: Verify that the comment count has changed to 1
+        cy.contains(postTitle)
+        .parents('div[class="home__main__feed__post__body"]')
+        .find('button')
+        .should('contain.text', '1')
+        })    
+
+    })
+    
+});
