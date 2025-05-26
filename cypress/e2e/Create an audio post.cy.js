c490c19ee5089863ef5d92e2230c44282c040dae
martinsaric <saricmartinposao@gmail.com>
2025-05-26 20:34:30 +0200

    test: new test like functionality is added

---
+++ cypress/e2e/Create an audio post.cy.js
@@ -0,0 +1,10 @@
+describe('Create an audio post', () => {
+
+    it('Verify that the user can create an audio post', () => {
+
+        cy.visit('/login')
+        cy.login('martinsaric94@gmail.com', 'constel123')
+
+    })
+    
+});
