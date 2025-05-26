9f66a9676f1e034050a95e0fc825444fd3516278
martinsaric <saricmartinposao@gmail.com>
2025-05-26 19:39:55 +0200

    chore: adjusting the code in test create a comment

--- cypress/e2e/Create a comment.cy.js
+++ cypress/e2e/Create a comment.cy.js
@@ -2,6 +2,7 @@ describe('Create a comment', () => {
 
     it('Verify that the user can create a comment', () => {
 
+
         //Step 1: Cypress command to login with the user credentials
         cy.login('martinsaric94@gmail.com', 'constel123')
 
@@ -9,15 +10,19 @@ describe('Create a comment', () => {
         cy.createNewPost().then(postTitle => {
             
         //Step 3: Cypress command to create a new comment
-        cy.createNewComment(postTitle)
+        cy.createNewComment(postTitle).then(uniqueComment => {
 
         //Step 4: Verify that the comment count has changed to 1
         cy.contains(postTitle)
         .parents('div[class="home__main__feed__post__body"]')
         .find('button')
         .should('contain.text', '1')
-        })    
 
+         //Step 5: Verify that the comment is visible on the page
+        cy.get('div[class="post__comments"]')
+        .should('contain.text', uniqueComment)
+        })   
+        })
     })
     
 });
