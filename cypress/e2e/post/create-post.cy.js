0713a20a6deb99db41bd3ca62c41bded56e78569
martinsaric <saricmartinposao@gmail.com>
2025-05-27 13:00:35 +0200

    chore: adjusting create-post test, adjusting Cypress commands

--- cypress/e2e/post/create-post.cy.js
+++ cypress/e2e/post/create-post.cy.js
@@ -2,21 +2,43 @@ describe('Creating new post', () => {
 
     it('Verify that the user can create a new post', () => {
         
+        //Step 1: Login with valid credentials
         cy.visit('/login')
         cy.login('martinsaric94@gmail.com', 'constel123')
 
+        //Step 2: Create logic to genereate random number for post title
         const randomNumber = Math.floor(Math.random() * 10000)
         const postTitle = `Test Post #${randomNumber}`
 
-
+        //Step 3: Create a new post
         cy.get('div[class="card-body"]')
         .should('be.visible')
         cy.get(`input[placeholder="What's happening"]`)
         .type(postTitle)
         cy.contains('button[type="button"]', 'New post').click()
 
+        //Step 4: Verify that the post is created
         cy.visit('/home')
         cy.contains(postTitle).should('be.visible')
+
+        //Step 5: Login with API to get token
+        cy.apiLogin().then(() => {
+            const token = Cypress.env('token')
+
+            //Step 6: Get the created post ID
+            cy.getApiPost(postTitle, token).then((myPost) => {
+                const postId = myPost.post_id
+
+                //Step 7: Delete the post
+                cy.apiDeletePost(postId, token).then((response) => {
+                    expect(response.status).to.eq(200)
+                })
+            })
+
+        })
+
+
+
     })
-    
-});
+
+})
