fbdf8c94a594b27587455007deadd8210b3a52da
martinsaric <saricmartinposao@gmail.com>
2025-05-27 17:01:06 +0200

    test: adjusted get-comments test

--- cypress/support/urls.js
+++ cypress/support/urls.js
@@ -1,8 +1,8 @@
-const baseApiURL = 'https://api.hr.constel.co/api/v1';
+const baseApiUrl = 'https://api.hr.constel.co/api/v1';
 
 export const apiURL = {
     login: `${baseApiUrl}/login`,
-    createPost: `${baseApiUrl}//posts`,
+    createPost: `${baseApiUrl}/posts`,
     getComments: (postId) => `${baseApiUrl}//posts/${postId}/comments`,
     likePost: (postId) => `${baseApiURL}/posts/${postId}/like`,
     createComment: (postId) => `${baseApiURL}/posts/${postId}/comments`
