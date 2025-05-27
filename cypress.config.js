c5b5bceac68cd4ab95bd9435e8dc118653689204
martinsaric <saricmartinposao@gmail.com>
2025-05-27 20:59:34 +0200

    test: added unhappy path for login form

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,14 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://constel-social-network.vercel.app/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
+     
 
-     specPattern: [
+
+    chromeWebSecurity: false,
+
+    specPattern: [
       'cypress/e2e/**/*.cy.{js,ts}',
       'cypress/api/**/*.cy.{js,ts}'
     ]
---
+++ cypress/e2e/unhappyPath/errors-validations-login.cy.js
@@ -0,0 +1,49 @@
+describe("Errors validations login", () => {
+  it("User login with empty email and valid password", () => {
+    cy.visit("/login");
+    cy.get('input[placeholder="Enter password here..."]').type("constel123");
+    cy.get('button[id="loginSubmitBtn"]').click();
+
+    cy.contains(
+      '[class="invalid-feedback"]',
+      "Email field is required."
+    ).should("be.visible");
+  });
+
+  it("User login with empty password and valid email", () => {
+    cy.visit("/login");
+    cy.get('input[placeholder="Enter email here..."]').type(
+      "martinsaric94@gmail.com"
+    );
+
+    cy.get('button[id="loginSubmitBtn"]').click();
+
+    cy.contains(
+      '[class="invalid-feedback"]',
+      "Password field is required."
+    ).should("be.visible");
+  });
+
+  it("User login with invalid credentials", () => {
+    cy.visit("/login");
+    cy.login("testing@user.com", "constel!!");
+
+    cy.get('button[id="loginSubmitBtn"]').click();
+
+    cy.contains('div[role="alert"]', "An error occurred during login.").should(
+      "be.visible"
+    );
+  });
+
+  it("User login with empty fields", () => {
+    cy.visit("/login");
+
+    cy.get('button[id="loginSubmitBtn"]').click();
+
+    cy.contains('[class="invalid-feedback"]', "Email field is required.");
+    cy.contains(
+      '[class="invalid-feedback"]',
+      "Password field is required."
+    ).should("be.visible");
+  });
+});
