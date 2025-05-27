894438dbf0af612e751f6ae14bc7e4af22b03926
martinsaric <saricmartinposao@gmail.com>
2025-05-27 21:13:55 +0200

    feat: added mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     baseUrl: "https://constel-social-network.vercel.app/",
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on, config) { 
+      require('cypress-mochawesome-reporter/plugin')(on);
       
     },
      
