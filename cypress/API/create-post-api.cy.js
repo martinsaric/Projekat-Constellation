63e35bc42e4adfc6da8aff25fc7cf552607d4a19
martinsaric <saricmartinposao@gmail.com>
2025-05-27 18:37:01 +0200

    char: formating api test

--- cypress/API/create-post-api.cy.js
+++ cypress/API/create-post-api.cy.js
@@ -1,19 +1,13 @@
-describe('Create post with API request', () => {
+describe("Create post with API request", () => {
+  it("Verify that the post can be created with API request", () => {
+    cy.apiLogin().then(() => {
+      const token = Cypress.env("token");
+      const randomNumber = Math.floor(Math.random() * 10000);
+      const postTitle = `Test post #${randomNumber}`;
 
-    it('Verify that the post can be created with API request', () => {
-
-        cy.apiLogin().then(() => {  
-
-            const token = Cypress.env('token')
-            const randomNumber = Math.floor(Math.random() * 10000)
-            const postTitle = `Test post #${randomNumber}`
-            
-            cy.apiCreatePost(postTitle).then((response) => {
-                expect(response.status).to.eq(200)
-            })
-        })
-       
-    
-
-    })
-})
+      cy.apiCreatePost(postTitle).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+  });
+});
--- cypress/API/login-api.cy.js
+++ cypress/API/login-api.cy.js
@@ -1,8 +1,6 @@
-describe('Login with API request', () => {
-
-    it('Verify that the user can login with API request', () => {
-
-        //Step 1: Login with API request
-        cy.apiLogin()
-    })
-})
+describe("Login with API request", () => {
+  it("Verify that the user can login with API request", () => {
+    //Step 1: Login with API request
+    cy.apiLogin();
+  });
+});
