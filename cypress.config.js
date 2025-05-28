972e13de515402273d1ca8148a7fb0cfe52ec6c7
DESKTOP-NG67NAN\Martin <saricmartinposao@gmail.com>
2025-05-28 10:43:26 +0200

    feat: adding and adjusting mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,28 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  //Added reporter to the config outside e2e
+  reporter: 'cypress-mochawesome-reporter',
+  
   e2e: {
-    reporter: 'cypress-mochawesome-reporter',
+    
     baseUrl: "https://constel-social-network.vercel.app/",
     setupNodeEvents(on, config) { 
+      //Another part of adding reported, inside of setupNodeEvents
       require('cypress-mochawesome-reporter/plugin')(on);
       
     },
      
+    video: true,
+    videoCompression: 32,  // Medium compression 
+    videoUploadOnPass: false,  // Video will not be upload if tests pass
+    screenshotOnRunFailure: true,  // Screenshot will be made if test fail
 
-
+    //To avoid web security and restrictions for smooth testing
     chromeWebSecurity: false,
 
+    //To see both folders on the cypress runner
     specPattern: [
       'cypress/e2e/**/*.cy.{js,ts}',
       'cypress/api/**/*.cy.{js,ts}'
