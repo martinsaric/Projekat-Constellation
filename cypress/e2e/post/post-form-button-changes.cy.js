16c8705bf0e8827dfbe984d3e8ca256776e26d1c
martinsaric <saricmartinposao@gmail.com>
2025-05-28 13:54:37 +0200

    chore: adjusting the login error validation test and post-form-button

--- cypress/e2e/post/post-form-button-changes.cy.js
+++ cypress/e2e/post/post-form-button-changes.cy.js
@@ -1,13 +1,20 @@
 describe("Post form button changes", () => {
-  beforeAll(() => {
+
+  //Step 1: Before all tests, set screen size to mobile
+  beforeEach(() => {
     cy.viewport(375, 812);
   });
 
   it("Verify that the ", () => {
+
+    //Step 2: Login with valid credentials
     cy.login("martinsaric94@gmail.com", "constel123");
 
+    //Step 3: Verify that the "Record audio" button is 
+    // visible and has the correct background color
     cy.contains('[type="button"]', "Record audio")
       .should("be.visible")
-      .and("have.css", "background-color", "rgb(21, 126, 255)");
+      .and("have.css", "background-color", "rgb(21, 126, 255)")
+      .and("have.text", "Record audio");
   });
 });
