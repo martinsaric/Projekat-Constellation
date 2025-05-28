6008b22eb1c1a1c9b5be260456bb6e8e1c08df0d
martinsaric <saricmartinposao@gmail.com>
2025-05-28 12:06:43 +0200

    test: login api test added validation

--- cypress/API/login-api.cy.js
+++ cypress/API/login-api.cy.js
@@ -1,6 +1,8 @@
 describe("Login with API request", () => {
   it("Verify that the user can login with API request", () => {
     //Step 1: Login with API request
-    cy.apiLogin();
+    cy.apiLogin().then((response) => {
+      expect(response.status).to.eq(200);
+    });
   });
 });
