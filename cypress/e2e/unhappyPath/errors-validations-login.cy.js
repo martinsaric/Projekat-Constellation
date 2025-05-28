4cc2b034447f280d2216675eec7520d069542a20
martinsaric <saricmartinposao@gmail.com>
2025-05-28 11:15:10 +0200

    test: added two new login error validations

--- cypress/e2e/unhappyPath/errors-validations-login.cy.js
+++ cypress/e2e/unhappyPath/errors-validations-login.cy.js
@@ -4,24 +4,18 @@ describe("Errors validations login", () => {
     cy.get('input[placeholder="Enter password here..."]').type("constel123");
     cy.get('button[id="loginSubmitBtn"]').click();
 
-    cy.contains(
-      '[class="invalid-feedback"]',
-      "Email field is required."
-    ).should("be.visible");
+    cy.contains('[class="invalid-feedback"]', "Email field is required.")
+    .should("be.visible");
   });
 
   it("User login with empty password and valid email", () => {
     cy.visit("/login");
-    cy.get('input[placeholder="Enter email here..."]').type(
-      "martinsaric94@gmail.com"
-    );
+    cy.get('input[placeholder="Enter email here..."]').type("martinsaric94@gmail.com");
 
     cy.get('button[id="loginSubmitBtn"]').click();
 
-    cy.contains(
-      '[class="invalid-feedback"]',
-      "Password field is required."
-    ).should("be.visible");
+    cy.contains('[class="invalid-feedback"]', "Password field is required.")
+    .should("be.visible");
   });
 
   it("User login with invalid credentials", () => {
@@ -30,9 +24,8 @@ describe("Errors validations login", () => {
 
     cy.get('button[id="loginSubmitBtn"]').click();
 
-    cy.contains('div[role="alert"]', "An error occurred during login.").should(
-      "be.visible"
-    );
+    cy.contains('div[role="alert"]', "An error occurred during login.")
+    .should("be.visible");
   });
 
   it("User login with empty fields", () => {
@@ -41,9 +34,29 @@ describe("Errors validations login", () => {
     cy.get('button[id="loginSubmitBtn"]').click();
 
     cy.contains('[class="invalid-feedback"]', "Email field is required.");
-    cy.contains(
-      '[class="invalid-feedback"]',
-      "Password field is required."
-    ).should("be.visible");
+    cy.contains('[class="invalid-feedback"]', "Password field is required.")
+    .should("be.visible");
+  });
+
+  it("Password field must have at least 6 characters", () => {
+    cy.visit("/login");
+
+    cy.get('input[placeholder="Enter password here..."]').type("const")
+    cy.get('button[id="loginSubmitBtn"]').click();
+
+    cy.contains('[class="invalid-feedback"]', "Please provide a minimum of 6 characters")
+    .should("be.visible")
+    .and("have.class", "invalid-feedback");
+  });
+
+  it("Email field only accepts valid email format", () => {
+    cy.visit("/login");
+
+    cy.get('input[placeholder="Enter email here..."]').type("martinsaric94@gmail.com");
+    cy.get('button[id="loginSubmitBtn"]').click();
+
+    cy.contains('[class="invalid-feedback"]', "Email format is not valid.")
+    .should("be.visible")
+    .and("have.class", "invalid-feedback");
   });
 });
